1b15e01eae2ce8347495e9e567cc9b3c079b02d9
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2022-01-04 20:55:36 -0800

    ima write this test for sure

--- src/components/notes/Note.jsx
+++ src/components/notes/Note.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import useAuth from '../../hooks/useAuth';
+import css from './NoteList.css';
 
 function Note({ note: {message} }) {
     const {user} = useAuth();
     return(
-        <><h2>{user.username}</h2><p>{message}</p></>
+        <><h4 className={css.noteTitle}>{user.username}</h4><p>{message}</p></>
     )
 }
 
