d132bb4fe6a11b14cd7d2721216896e21251e2a7
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2022-01-04 21:05:22 -0800

    snap passes, next time on unneccesary commits...UI testing

---
+++ src/views/Auth/Login.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Login from "./Login"
+import { screen, render } from '@testing-library/react';
+import { AuthProvider } from '../../context/AuthCtx';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+it('renders log in form and does other cool shit too', () => {
+  const {container} = render (
+    <AuthProvider>
+      <Router>
+        <Login />
+      </Router>
+    </AuthProvider>
+  );
+
+  expect(container).toMatchSnapshot();
+})
